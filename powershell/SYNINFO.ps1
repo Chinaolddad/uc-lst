63927d358c9c2062499cec080c01beb438ff225e
Chinaolddad <653873407@qq.com>
2022-01-25 19:19:46 -0500

    piwershell files

---
+++ powershell/SYNINFO.ps1
@@ -0,0 +1,7 @@
+﻿function getIP{
+(Get-NetIPAddress).IPv4Address | select-string "192"
+}
+write-Host(getIP)
+$IP = getIP
+write-Host("This is out machine's IP :$IP")
+write-Host("This machine's IP is {0}" -f $IP)
